8a7e18fb2b4597b78f2465f101d7a1a13ddc5b63
aspire2aspire <aspiretoaspire@gmail.com>
2024-01-03 11:36:16 -0600

    Update gematriaNGG.js

--- calc/gematriaNGG.js
+++ calc/gematriaNGG.js
@@ -1,6 +1,6 @@
 var catArr = []; gemArr = []
 var cipherArray = [];
-var openCiphers = ["English Ordinal", "Full Reduction", "Reverse Ordinal", "Reverse Full Reduction"]
+var openCiphers = ["English Ordinal", "Full Reduction", "Reverse Ordinal", "Reverse Full Reduction", "Chaldean"]
 var ciphersOn = []; allCiphers = []; sHistory = []
 var opt_NumCalculation = "Reduced"
 var primeArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 
