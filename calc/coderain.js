ee2f280bb5cdf9ba8075d9f1e83a0175d7ab665d
aspire2aspire <aspiretoaspire@gmail.com>
2025-03-05 22:04:08 -0600

    Update coderain.js

--- calc/coderain.js
+++ calc/coderain.js
@@ -1,84 +1,65 @@
-// ================ Matrix code rain (HTML5 canvas) =================
+// Matrix code rain (HTML5 canvas)
 
-function initCodeRain() {
+var height_html = $(window).height();
 
-	height_html = $(window).height()
+const canvas = document.getElementById("canv");
+const ctx = canvas.getContext("2d");
 
-	canvas = document.getElementById("canv")
-	ctx = canvas.getContext("2d")
+// set the width and height of the canvas
+const w = canvas.width = document.body.offsetWidth;
+const h = canvas.height = height_html;
 
-	if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) $('#canv').css({'filter':'blur(1px)'}) // blur effect for Firefox
+// draw a plain color rectangle of width and height same as that of the canvas
+ctx.fillStyle = '#202020';
+ctx.fillRect(0, 0, w, h);
 
-	// set width and height of the canvas
-	w = canvas.width = document.body.offsetWidth
-	h = canvas.height = height_html
+const cols = Math.floor(w / 20) + 1;
+const ypos = Array(cols).fill(0);
 
-	// draw a plain color rectangle of width and height same as that of the canvas
-	ctx.fillStyle = "hsl("+interfaceHue+","+(22*interfaceSat)+"%,"+(16*interfaceLit)+"%)" // CSS var(--body-bg-accent)
-	ctx.fillRect(0, 0, w, h)
-
-	cols = Math.floor(w / 14) + 1 // px
-	ypos = Array(cols).fill(0)
-}
-
+var code_rain; // var to clear interval
+var code_rain_active = false; // current state (bugfix for 'Option' menu)
+if (!opt_MatrixCodeRain) code_rain_active = true; // if code rain is initially disabled
+  
 function matrix() {
 
-	// draw a semitransparent black rectangle on top of previous drawing
-	ctx.fillStyle = "#00000010"
-	if(navigator.userAgent.toLowerCase().indexOf('firefox') == -1) { // if not Firefox
-		// ctx.shadowColor = "rgba(0,0,0,0)" // reset blurred shadows for old characters
-		ctx.shadowColor = "hsla(0,0%,0%,0.0)" // reset blurred shadows for old characters
-		ctx.shadowBlur = 0 // reset blurred shadows
-	}
-	ctx.fillRect(0, 0, w, h)
+	// Draw a semitransparent black rectangle on top of previous drawing
+	ctx.fillStyle = "#0001"; // rgba(0,0,0,0.04) - leaves traces of dark rows
+	ctx.fillRect(0, 0, w, h);
 
-	// set color and font in the drawing context
-	ctx.fillStyle = "hsl("+coderainHue+","+(coderainSat*100)+"%,"+(coderainLit*100)+"%)"
-	ctx.font = "bold 18pt matrix-font"
-	if(navigator.userAgent.toLowerCase().indexOf('firefox') == -1) { // if not Firefox
-		ctx.shadowColor = "hsla("+coderainHue+",100%,50%,0.4)"
-		ctx.shadowBlur = 4
-	}
+	// Set color to green and font to 15pt monospace in the drawing context
+	ctx.fillStyle = "#004400";
+	ctx.font = "15pt Moon, monospace";
 
-	var matrixChars = [97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,
-		48,49,50,51,52,53,54,55,56,57,36,43,45,42,47,61,37,34,39,35,38,95,40,41,44,46,59,58,63,33,92,124,123,125,60,62,91,93,94,126]
-	var aLen = matrixChars.length // glyphs from matrix font
-	
 	// for each column put a random character at the end
 	ypos.forEach((y, ind) => {
-
-		// choose a random character from array
-		text = String.fromCharCode(matrixChars[rndInt(0,aLen-1)])
+		// generate a random character
+		//const text = Math.floor(Math.random() * 10);
+		const text = String.fromCharCode(Math.random() * 128);
 
 		// x coordinate of the column, y coordinate is already given
-		x = ind * 14 // px
+		const x = ind * 20;
 		// render the character at (x, y)
-		ctx.fillText(text, x, y)
+		ctx.fillText(text, x, y);
 
 		// randomly reset the end of the column if it's at least 100px high
-		if (y > 100 + Math.random() * 10000) ypos[ind] = 0
-		// otherwise just move the y coordinate for the column 20px down
-		else ypos[ind] = y + 21 // px
+		if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+		// otherwise just move the y coordinate for the column 20px down,
+		else ypos[ind] = y + 20;
 	});
 }
 
-function rndInt(min, max) { // inclusive
-	return Math.floor(Math.random()*(max-min+1)+min)
-}
-
-function toggleCodeRain() {
-	if (optMatrixCodeRain) {
-		clearInterval(code_rain) // reset previous instance
-		document.getElementById("canv").style.display = "none"
-		initCodeRain() // recalculate canvas size
+function toggle_code_rain() {
+	if (opt_MatrixCodeRain && !code_rain_active) {
 		code_rain = setInterval(matrix, 50)
 		document.getElementById("canv").style.display = ""
-		return
-	} else {
-		clearInterval(code_rain)
+		code_rain_active = true
+	} else if (!opt_MatrixCodeRain && code_rain_active) {
+		clearInterval(code_rain);
+		ctx.fillStyle = "#202020" // static bg
+		ctx.fillRect(0, 0, w, h)
 		document.getElementById("canv").style.display = "none"
-		return
+		code_rain_active = false
 	}
 }
 
-toggleCodeRain()
+toggle_code_rain(); // check if enabled by default on page load
